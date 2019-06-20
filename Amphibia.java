303d4ff1ab383725bb556402b9d2c77390d84113
snowdenplayer <51150061+snowdenplayer@users.noreply.github.com>
2019-06-20 16:57:12 +0300

    Add files via upload

---
+++ Amphibia.java
@@ -0,0 +1,18 @@
+public class Amphibia {
+
+    public void eat(){
+        System.out.println("Зараз тварина ъсть");
+    }
+
+    public void sleep(){
+        System.out.println("Зараз тварина спить");
+    }
+
+    public void swim(){
+        System.out.println("Зараз тварина плаваэ");
+    }
+
+    public void walk(){
+        System.out.println("Зараз тварина гуляэ");
+    }
+}
---
+++ Application.java
@@ -0,0 +1,9 @@
+public class Application {
+    public static void main(String[] args) {
+        Frog klava = new Frog();
+        klava.eat();
+        klava.sleep();
+        klava.swim();
+        klava.walk();
+    }
+}
